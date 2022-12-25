09ef672f0369e82c4334150c13e5a35634042df9
Josef Friedrich <josef@friedrich.rocks>
2022-12-25 18:42:25 +0100

    Try entrypoints with poetry

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
-pygmentize -l ./jf_pygments/sql_lexer.py:BaldrSqlLexer -x tests/test.sql
+pygmentize -l ./jf_pygments/lexers/sql.py:BaldrSqlLexer -x tests/test.sql
+
+pygmentize -l baldrsql tests/test.sql
